7af36682e74a4b88146a94ac0cf04f47144c76e9
Darus Salam <darussalam66d@gmail.com>
2025-11-04 08:20:56 +0600

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: kPrimaryGradient
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/image1.png',height: 120,),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryGradient = LinearGradient(
+    colors: [
+      Color(0xFF6C63FF),
+      Color(0xFF9D4EDD)
+    ],
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter
+);
