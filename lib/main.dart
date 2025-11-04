c4ed7cb088b46bd6e54ad1ee65924752871d6e15
Darus Salam <darussalam66d@gmail.com>
2025-11-04 07:42:17 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/home_screen.dart';
+
+void main(){
+  runApp( const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
